447c0da15a496e74f92ca8e6ea984e55df2381a3
FedericoManzano <federicomanzano111@gmail.com>
2017-11-29 21:29:01 -0300

    Validacion de usuario

--- Desarrollo/cgp-2017-cliente/src/main/java/com/federico/chat/eventos/EventoAbrirVentana.java
+++ Desarrollo/cgp-2017-cliente/src/main/java/com/federico/chat/eventos/EventoAbrirVentana.java
@@ -3,6 +3,8 @@ package com.federico.chat.eventos;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
 import com.federico.chat.chat.Chat;
 import com.federico.chat.modelos.Conversacion;
 
@@ -19,9 +21,13 @@ public class EventoAbrirVentana implements ActionListener{
 		String usuarioSeleccionado = chat.getMenuGeneral().dameSeleccionado();
 		for(Conversacion con : Chat.listadoConectados) {
 			if(con.getUsuarioExterno().equals(usuarioSeleccionado)) {
-				con.getMenuPrivado().setVisible(true);
+				if(!usuarioSeleccionado.equals(con.getUsuarioInterno())) {
+					con.getMenuPrivado().setVisible(true);
+				}else{
+					JOptionPane.showMessageDialog(con.getMenuPrivado(), "No puede enviarse mensajes a usted mismo");
+				}
+				
 			}
 		}
 	}
-
 }
